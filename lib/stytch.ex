5d70f12d6606d6f9f35f8e96d48d4b0797be2451
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2025-06-27 13:17:08 -0400

    Add JWKS handler for verifying session JWTs

--- lib/stytch.ex
+++ lib/stytch.ex
@@ -13,4 +13,27 @@ defmodule Stytch do
   def decode_webhook(payload) do
     Stytch.Decoder.decode_direct(payload, {Stytch.WebhookPayload, :t})
   end
+
+  @doc """
+  Verify a signed JWT using a key set provided by Stytch
+
+  Returns the payload of the signed JWT if successful, or an error if there is no matching key.
+  It is the caller's responsibility to check the expiration time (`exp` in the payload).
+
+  In order to use this function, you must first start `Stytch.JWKS` in your application
+  supervision tree. This process automatically fetches and caches the JWKS (JSON Web Key Set)
+  provided by Stytch. If you need support for multiple projects, you can start multiple instances
+  of the `Stytch.JWKS` module with different names.
+
+  ## Options
+
+    * `name`: The name of the key set to use (defaults to `Stytch.JWKS`). This should match the
+      name of the `Stytch.JWKS` process in your application supervision tree.
+
+  """
+  @spec verify_jwt(String.t()) :: {:ok, map} | {:error, %RuntimeError{}}
+  @spec verify_jwt(String.t(), keyword) :: {:ok, map} | {:error, %RuntimeError{}}
+  def verify_jwt(jwt, opts \\ []) do
+    Stytch.JWKS.verify(jwt, opts)
+  end
 end
---
+++ test/stytch/jwks_test.exs
@@ -0,0 +1,32 @@
+defmodule Stytch.JWKSTest do
+  use ExUnit.Case, async: true
+
+  alias Stytch.JWKS
+
+  @valid_jwt "eyJhbGciOiJSUzI1NiIsImtpZCI6Imp3ay10ZXN0LTk2YWM0NjE0LWZlMzQtNDBhNS05YzJlLWE1ZTk4YThmYTU1ZiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicHJvamVjdC10ZXN0LWYyMDJlZGIwLTNmNDgtNDUyYi1hNmRmLTgzYjUxNjM0MmJiZSJdLCJleHAiOjE3NTA5OTY1OTIsImh0dHBzOi8vc3R5dGNoLmNvbS9vcmdhbml6YXRpb24iOnsib3JnYW5pemF0aW9uX2lkIjoib3JnYW5pemF0aW9uLXRlc3QtYTBhZmVhMGYtMGRiZS00ZWY1LTg4YmYtN2U5ZTdhMDZkYWI4Iiwic2x1ZyI6InRvZ2V0aGVyIn0sImh0dHBzOi8vc3R5dGNoLmNvbS9zZXNzaW9uIjp7ImlkIjoibWVtYmVyLXNlc3Npb24tdGVzdC0zYTQyOTUxNi0zYmNlLTQ1YmItYjY3OS1mYWVmZGRjZWRjZmEiLCJzdGFydGVkX2F0IjoiMjAyNS0wNi0yN1QwMzo1MTozMloiLCJsYXN0X2FjY2Vzc2VkX2F0IjoiMjAyNS0wNi0yN1QwMzo1MTozMloiLCJleHBpcmVzX2F0IjoiMjAyNS0wNi0yN1QwNDo1MTozMloiLCJhdHRyaWJ1dGVzIjp7InVzZXJfYWdlbnQiOiIiLCJpcF9hZGRyZXNzIjoiIn0sImF1dGhlbnRpY2F0aW9uX2ZhY3RvcnMiOlt7InR5cGUiOiJzc28iLCJkZWxpdmVyeV9tZXRob2QiOiJzc29fb2lkYyIsImxhc3RfYXV0aGVudGljYXRlZF9hdCI6IjIwMjUtMDYtMjdUMDM6NTE6MzJaIiwib2lkY19zc29fZmFjdG9yIjp7ImlkIjoib2lkYy1tZW1iZXItcmVnaXN0cmF0aW9uLXRlc3QtMTBmNGNkODctNTg1OC00MTZkLWI1MGItZGM2MTM3YWZkNzgzIiwicHJvdmlkZXJfaWQiOiJvaWRjLWNvbm5lY3Rpb24tdGVzdC0zYzA4NjY3MC1hYmI5LTQzMjktYmNlOC0wZTJmYzlhZDUyNDMiLCJleHRlcm5hbF9pZCI6IjAwdXNwdzJ0ZjU2dkJBejBZNjk3In19XSwicm9sZXMiOlsic3R5dGNoX21lbWJlciJdfSwiaWF0IjoxNzUwOTk2MjkyLCJpc3MiOiJzdHl0Y2guY29tL3Byb2plY3QtdGVzdC1mMjAyZWRiMC0zZjQ4LTQ1MmItYTZkZi04M2I1MTYzNDJiYmUiLCJuYmYiOjE3NTA5OTYyOTIsInN1YiI6Im1lbWJlci10ZXN0LTVkZTAxMTA5LWJhZTEtNGE0ZC05MDhmLWY3YTI2NzIwZjRmMiJ9.qvKGZYvTM1endd2uELkGewhoJZ8z-gWcuPwdIRHESSfDisoEZo4OW02NFbeoOiBKNqzjG3iLrdnrMFj3xYBcDWeascqt2YeeY1sKS5xe7OZp7TkMUT5SOB51U_sXftNCH9AGUe6U2hliHzJSv36xQidyWAxhVFwnl9IWXg3A4g1NzIisIwofJWldgEltFud-982HznKHPcQYOE3UyDvh_XS8ubxaQ__J7JcUckXv5lmASgwCITzaTIEoX3G4Wpt0K4QJvAnv7rUIgpkzbg_lePRImGDzSurj6yDKMwWwMzS5o7ZDKyrxsEO1yOk7Bs1WfF5FW6LF9utpfeEXWVldkw"
+
+  @valid_jwk %{
+    "alg" => "RS256",
+    "e" => "AQAB",
+    "key_ops" => ["verify"],
+    "kid" => "jwk-test-96ac4614-fe34-40a5-9c2e-a5e98a8fa55f",
+    "kty" => "RSA",
+    "n" =>
+      "wBQvcXVISfN9YQc1G3BwvmII73z7cshOwJFheHNr-WNO5pPJfVjkjuZ3vWgZHDGmdJKXuXtTR2MJdJXBhDnV2EeAY7qqQDChSGCNFzxNLj2Rovg5xeRhyRLr3wAbz_6Bme9KDtDqdQkt7bZJF1rwkqINQg7QKLdTxhgnulfM5gPgAH6af9-WwYSB94CrWS5cVH149hq3Y9mFNK5-u2AVJmOlmvCRFHfJ3PCEeiWwbJH_mCigxGLA6zsdJgEnwywwWsv6SOb99YygNtBjv3jKTI21tLs_tbBGsrlvdXO38i-4i97actm1X2vF80XBiMfp3gJBuA_BRQPb7pNEo6ZXSw",
+    "use" => "sig",
+    "x5c" => [
+      "MIICvTCCAaWgAwIBAgIBATANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQKEwZTdHl0Y2gwHhcNMjUwNjI3MDM1NDE2WhcNMzUwNjI3MDM1NDE2WjARMQ8wDQYDVQQKEwZTdHl0Y2gwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDAFC9xdUhJ831hBzUbcHC+YgjvfPtyyE7AkWF4c2v5Y07mk8l9WOSO5ne9aBkcMaZ0kpe5e1NHYwl0lcGEOdXYR4BjuqpAMKFIYI0XPE0uPZGi+DnF5GHJEuvfABvP/oGZ70oO0Op1CS3ttkkXWvCSog1CDtAot1PGGCe6V8zmA+AAfpp/35bBhIH3gKtZLlxUfXj2Grdj2YU0rn67YBUmY6Wa8JEUd8nc8IR6JbBskf+YKKDEYsDrOx0mASfDLDBay/pI5v31jKA20GO/eMpMjbW0uz+1sEayuW91c7fyL7iL3tpy2bVfa8XzRcGIx+neAkG4D8FFA9vuk0SjpldLAgMBAAGjIDAeMA4GA1UdDwEB/wQEAwIHgDAMBgNVHRMBAf8EAjAAMA0GCSqGSIb3DQEBCwUAA4IBAQCqhGBtzl2H0+nPK+ZsIiDOc1K2G6HfbmdfhUd/q/9p3KjQ+bFB5aJ6S6pySA0uXLPE1jikdoX0ZEbJ8Zguy5iwUmFj7ZNco1zIQsVIyHyGnPao32Qhki0n6k0J15tPlQD18kW/ji4tAqZECHdoksFOtrMPaDC1JcnpYlknyhAubRUmIXaEmHEZvhjuLNVxC/upAXKhynvdJZafE4XpK55WqD+BiaCQh5nL3ZsJLcVPdx8k7D8Tycq9sG4nAuBpNo9dXTjdNNISxwLysi5LjjOk9XGqWUWyjKlXEuH7PO3g2Gr64mCenFJuaOM8shIQJIFlH8scGUVdyz+y8+lIv9vz"
+    ]
+  }
+
+  describe "verify/2" do
+    test "verifies a signed JWT" do
+      module = Module.concat([Stytch.JWKS, "Test#{System.unique_integer([:positive])}"])
+      jwk = JOSE.JWK.from(@valid_jwk)
+      Application.put_env(:stytch, module, jwks: [jwk])
+
+      assert {:ok, %{"sub" => "member-test-5de01109-bae1-4a4d-908f-f7a26720f4f2"}} =
+               JWKS.verify(@valid_jwt, name: module)
+    end
+  end
+end
