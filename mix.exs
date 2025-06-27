60139689a8d333f2c654ab3897ae5be9b9ee96ce
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2025-06-27 13:52:17 -0400

    Add CI and cache/docs workflows

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule Stytch.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      docs: docs(),
+      name: "Stytch Elixir SDK",
+      source_url: "https://github.com/togethercomputer/stytch-elixir",
+      homepage_url: "https://github.com/togethercomputer/stytch-elixir"
     ]
   end
 
@@ -19,10 +25,26 @@ defmodule Stytch.MixProject do
 
   defp deps do
     [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.38", only: [:dev], runtime: false},
       {:jose, "~> 1.11"},
       {:oapi_generator, github: "aj-foster/open-api-generator", branch: "main"},
       {:req, "~> 0.5"},
       {:req_telemetry, "~> 0.1"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: [
+        "README.md": [title: "Introduction"],
+        "CODE_OF_CONDUCT.md": [title: "Code of Conduct"],
+        "CONTRIBUTING.md": [title: "Contributing"],
+        LICENSE: [title: "License"]
+      ],
+      formatters: ["html"],
+      source_ref: "main"
     ]
   end
 end
