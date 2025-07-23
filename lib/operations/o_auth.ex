67a705cf2a3991323988f516372badd549e6b1ba
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2025-07-23 18:00:45 -0400

    Add typed response for OAuth authenticate endpoint

--- lib/operations/o_auth.ex
+++ lib/operations/o_auth.ex
@@ -5,10 +5,39 @@ defmodule Stytch.OAuth do
 
   @default_client Stytch.Client
 
+  @type authenticate_200_json_resp :: %{
+          intermediate_session_token: String.t(),
+          member: Stytch.Member.t(),
+          member_authenticated: boolean,
+          member_id: String.t(),
+          member_session: Stytch.Member.Session.t() | nil,
+          mfa_required: Stytch.MFARequired.t() | nil,
+          organization: Stytch.Organization.t(),
+          organization_id: String.t(),
+          primary_required: Stytch.PrimaryRequired.t() | nil,
+          provider_subject: String.t(),
+          provider_type: String.t(),
+          provider_values: Stytch.OAuth.authenticate_200_json_resp_provider_values() | nil,
+          request_id: String.t(),
+          reset_sessions: boolean,
+          session_jwt: String.t(),
+          session_token: String.t(),
+          status_code: integer
+        }
+
+  @type authenticate_200_json_resp_provider_values :: %{
+          access_token: String.t() | nil,
+          expires_at: String.t() | nil,
+          id_token: String.t() | nil,
+          refresh_token: String.t() | nil,
+          scopes: [String.t()] | nil
+        }
+
   @doc """
   Authenticate OAuth
   """
-  @spec authenticate(body :: map, opts :: keyword) :: {:ok, map} | {:error, Stytch.Error.t()}
+  @spec authenticate(body :: map, opts :: keyword) ::
+          {:ok, Stytch.OAuth.authenticate_200_json_resp()} | {:error, Stytch.Error.t()}
   def authenticate(body, opts \\ []) do
     client = opts[:client] || @default_client
 
@@ -19,7 +48,10 @@ defmodule Stytch.OAuth do
       body: body,
       method: :post,
       request: [{"application/json", :map}],
-      response: [{200, :map}, default: {Stytch.ErrorResponse, :t}],
+      response: [
+        {200, {Stytch.OAuth, :authenticate_200_json_resp}},
+        default: {Stytch.ErrorResponse, :t}
+      ],
       opts: opts
     })
   end
@@ -259,6 +291,38 @@ defmodule Stytch.OAuth do
 
   @doc false
   @spec __fields__(atom) :: keyword
+  def __fields__(:authenticate_200_json_resp) do
+    [
+      intermediate_session_token: {:string, :generic},
+      member: {Stytch.Member, :t},
+      member_authenticated: :boolean,
+      member_id: {:string, :generic},
+      member_session: {Stytch.Member.Session, :t},
+      mfa_required: {Stytch.MFARequired, :t},
+      organization: {Stytch.Organization, :t},
+      organization_id: {:string, :generic},
+      primary_required: {Stytch.PrimaryRequired, :t},
+      provider_subject: {:string, :generic},
+      provider_type: {:string, :generic},
+      provider_values: {Stytch.OAuth, :authenticate_200_json_resp_provider_values},
+      request_id: {:string, :generic},
+      reset_sessions: :boolean,
+      session_jwt: {:string, :generic},
+      session_token: {:string, :generic},
+      status_code: :integer
+    ]
+  end
+
+  def __fields__(:authenticate_200_json_resp_provider_values) do
+    [
+      access_token: {:string, :generic},
+      expires_at: {:string, :generic},
+      id_token: {:string, :generic},
+      refresh_token: {:string, :generic},
+      scopes: [string: :generic]
+    ]
+  end
+
   def __fields__(:login_github_307_json_resp) do
     [redirect_url: {:string, :generic}, request_id: {:string, :generic}, status_code: :integer]
   end
